8b74724546d097e327489a5a58fcb80f0da68a03
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2015-11-24 00:54:54 +0000

    - Control Points loaded - Vertex indices loaded - Control points 'ortho' projected on camera transform

--- FbxCloudProcessor/FbxCloudProcessor.py
+++ FbxCloudProcessor/FbxCloudProcessor.py
@@ -6,8 +6,6 @@ import sys
 import fbx
 import urllib2
 
-
-# example of a python class
  
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler): 
 
@@ -20,7 +18,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     manager = fbx.FbxManager.Create()
     importer = fbx.FbxImporter.Create(manager, 'myImporter')
 
-    req = urllib2.Request('https://www.dropbox.com/s/48fpvxdvk4l4qv2/cubeMan.fbx?dl=1')
+    """req = urllib2.Request('https://www.dropbox.com/s/48fpvxdvk4l4qv2/cubeMan.fbx?dl=1')
     req.add_header('Content-Type', 'application/octet-stream')
     r = urllib2.urlopen(req)
 
@@ -29,10 +27,11 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     newFile = open ("object.fbx", 'wb')
     newFile.write(object);
-    newFile.close()
+    newFile.close()"""
             
 
-    status = importer.Initialize("object.fbx")
+    """status = importer.Initialize("object.fbx")"""
+    status = importer.Initialize("TestMesh.FBX")
     if status == False :
       print 'fbx initialization failed'
       print 'Error: %s' % importer.GetLastErrorString()
@@ -44,31 +43,79 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     
     root = scene.GetRootNode() 
-    
-    
-    vertexCount = 0
 
-    def countVertex (node) :
-        result = 0
-        mesh = node.GetMesh()
-        if(mesh != None) :
-            result += mesh.GetPolygonVertexCount()
+    """MATHS"""
+
+    def transposeMatrix (matrix) :
+
+        """from 'python cookbook' """
+        t = [[r[col] for r in matrix] for col in range(len(matrix[0]))] 
+
+        return t
+
+    def vectorDotMatrix (vector, matrix) :
+
+        result = [0] * 4
+        for i in range(4) :
+            result[i] = vector[0] * matrix[0][i] + vector[1] * matrix[1][i] + vector[2] * matrix[2][i] + vector[3] * matrix[3][i]
+        
+        return result
+
+    """END MATHS"""
+
+    cameraToWorld = [[ 1, 0, 0, 0],
+                     [ 0, 1, 0, 0],
+                     [ 0, 0, 1, 0],
+                     [ 0, 0, 0, 0]]
+    worldToCamera = transposeMatrix(cameraToWorld)
+
+    controlPoints = [];
+    vertexIndicesArray = []
+    def getVertices (node) :
+
+        mesh = node.GetMesh()        
+        if(mesh != None and mesh.IsTriangleMesh()) :
+
+            cPoints = mesh.GetControlPoints();
+            count = list(range(len(cPoints)))
+            for i in count:
+                controlPoints.append(cPoints[i])
+
+            polygonCount = mesh.GetPolygonCount()
+            count = list(range(polygonCount))
+            for i in count:       
+                """Check if the polygon is facing the camera, discard it if not"""                         
+                vertexIndicesArray.append(mesh.GetPolygonVertices())    
+                             
         childNumber = node.GetChildCount()
         count = list(range(childNumber))
         for i in count:
-            result += countVertex(node.GetChild(i)) 
-        return result
-       
-    vertexCount = countVertex(root)    
+            getVertices(node.GetChild(i))   
+                           
+        return     
+    
+    def projectVertices () :
+
+        count = range(len(controlPoints))
+        for i in count:       
+            """controlPoints world->camera space"""                         
+            controlPoints[i] = vectorDotMatrix(controlPoints[i], worldToCamera)
+            """orthographic projection - get rid of z component"""
+            controlPoints[i] = [controlPoints[i][0], controlPoints[i][1]]
+        return  
+
+    """Main"""
+    getVertices(root)    
+    projectVertices()
 
     s.send_response(200)
     s.send_header("Content-type", "text/html")
     s.end_headers()
     s.wfile.write("<html><head><title>FBX online counter</title></head>")
     s.wfile.write("<body><p>Put the fbx file path in the URL</p>")
-    s.wfile.write("<p>Vertex count: %s</p>" % (vertexCount))
+    """s.wfile.write("<p>Vertex count: %s</p>" % (vertexCount))"""
     
     s.wfile.write("</body></html>")
 
-httpd = BaseHTTPServer.HTTPServer(("0.0.0.0", 8000), MyHandler)
+httpd = BaseHTTPServer.HTTPServer(("localhost", 8000), MyHandler)
 httpd.serve_forever()
